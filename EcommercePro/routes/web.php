41cca9d64af71c5c8225713d59dea74043036db4
khanhphamK2 <20521453@mg.uit.edu.vn>
2022-10-07 17:59:16 +0700

    checkoutbranch

---
+++ EcommercePro/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::get('/redirect', [HomeController::class, 'redirect']);
+
+Route::get('/view_catagory', [AdminController::class, 'view_catagory']);
+Route::post('/add_catagory', [AdminController::class, 'add_catagory']);
+Route::get('/delete_catagory/{id}', [AdminController::class, 'delete_catagory']);
+Route::get('/view_product', [AdminController::class, 'view_product']);
